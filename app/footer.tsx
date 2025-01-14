149a4c0e7477f81be01f1f90ec8f20eaf4e38b83
vanshanand34 <anandvansh34@gmail.com>
2025-01-14 23:11:41 +0530

    improved structure and fixed the issue causing no image in case of multiple dot clicks

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,28 +1,33 @@
-export default function Footer({ handler, count = 3 }: { handler: (index: number) => void, count: number }) {
-    const arr = Array.from({ length: count-1 });
+import { JSX } from "react";
+
+export default function Footer({
+    handler,
+    count = 3,
+}: {
+    handler: (index: number) => void;
+    count?: number;
+}): JSX.Element {
+    const dotElements = Array.from({ length: count - 1 }).map((_, index) => {
+        return (
+            <div
+                className="w-[2vw] h-[2vw] md:w-[1vw] md:h-[1vw] rounded-[50%] bg-white cursor-pointer"
+                onClick={() => handler(index + 2)}
+                id={`dot-${index + 2}`}
+                key={index + 2}>
+            </div>
+        );
+    });
     return (
         <div className="absolute left-[45vw] right-[45vw] w-[10vw] md:w-[10vw] top-[80vh]">
             <div className="flex justify-between">
-            <div
-                className="w-[2vw] h-[2vw] md:w-[1vw] md:h-[1vw] rounded-[50%] bg-black cursor-pointer"
-                onClick={() => handler(1)}
-                id={`dot-1`}
-                key={1}>
-            </div>
-                {arr.map((item, index) => (
-                    <div
-                        className="w-[2vw] h-[2vw] md:w-[1vw] md:h-[1vw] rounded-[50%] bg-white cursor-pointer"
-                        onClick={() => handler(index + 2)}
-                        id={`dot-${index + 2}`}
-                        key={index + 2}>
-                    </div>
-                ))}
+                <div
+                    className="w-[2vw] h-[2vw] md:w-[1vw] md:h-[1vw] rounded-[50%] bg-black cursor-pointer"
+                    onClick={() => handler(1)}
+                    id={`dot-1`}
+                    key={1}>
+                </div>
+                {dotElements}
             </div>
         </div>
     );
 }
-// function DotElement({ index, handler }: { index: number, handler: (index: number) => void }) {
-//     return (
-        
-//     );
-// }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
+import { JSX } from "react";
 import { useState } from "react"
 import Image1 from "@assets/glow-spaceship.avif";
 import Image2 from "@assets/man-in-space.jpg";
@@ -8,21 +9,22 @@ import Image3 from "@assets/some-planet.png";
 import Image4 from "@assets/shivji-image.jpg";
 import Footer from "./footer";
 
-const Home = () => {
+const Home = (): JSX.Element => {
 
   const [currImage, setCurrImage] = useState(1);
   const numberOfImages = 3;
 
   const displayHideImage = (indexToDisplay: number, indexToHide: number) => {
-    document.getElementById(`image-${indexToDisplay}`)?.classList.remove("hidden");
     document.getElementById(`image-${indexToHide}`)?.classList.add("hidden");
+    document.getElementById(`image-${indexToDisplay}`)?.classList.remove("hidden");
     const dotToDisplay = document.getElementById(`dot-${indexToDisplay}`);
     const dotToHide = document.getElementById(`dot-${indexToHide}`)
 
-    dotToDisplay?.classList.add("bg-black");
-    dotToDisplay?.classList.remove("bg-white");
+    console.log("inside, ", dotToDisplay, dotToHide);
     dotToHide?.classList.add("bg-white");
     dotToHide?.classList.remove("bg-black");
+    dotToDisplay?.classList.add("bg-black");
+    dotToDisplay?.classList.remove("bg-white");
   }
 
 
@@ -46,55 +48,47 @@ const Home = () => {
   return (
     <div className="flex">
       <div className="image" id="image-1">
-        <Image
-          src={Image2}
-          style={{
-            width: "100vw",
-            height: "100vh",
-            minWidth: "100vw",
-            objectFit: "cover"
-          }}
-          alt="Space picture"
-        ></Image>
+        <FullSizeImage imageSrc={Image2} altText="Space picture" />
       </div>
       <div className="image hidden" id="image-2">
-        <Image
-          src={Image3}
-          style={{
-            width: "100vw",
-            height: "100vh",
-            minWidth: "100vw",
-            objectFit: "cover"
-          }}
-          alt="Space picture"
-        ></Image>
+        <FullSizeImage imageSrc={Image3} altText="Space picture" />
       </div>
       <div className="image hidden" id="image-3">
-        <Image
-          src={Image4}
-          style={{
-            width: "100vw",
-            height: "100vh",
-            minWidth: "100vw",
-            objectFit: "cover"
-          }}
-          alt="Space picture"
-        ></Image>
+        <FullSizeImage imageSrc={Image4} altText="Space picture" />
       </div>
+
       <button
         className="absolute top-[46%] left-[5%] bg-black shadow-[3px_5px_18px_rgb(255,255,255,0.3)] text-white rounded-full md:py-2 md:px-5 py-1 px-3 text-xl md:text-3xl hover:bg-white hover:text-black font-[family-name:var(--font-geist-mono)]"
         onClick={showPrevImage}
-
       >&#10094;
       </button>
+
       <button
         className="absolute top-[46%] right-[5%] bg-black shadow-[3px_5px_18px_rgb(255,255,255,0.3)] text-white rounded-full md:px-5 md:py-2 py-1 px-3 text-xl md:text-3xl hover:bg-white hover:text-black font-[family-name:var(--font-geist-mono)]"
         onClick={showNextImage}
       >&#10095;
       </button>
+
       <Footer count={3} handler={setImageFromDot} />
     </div>
   );
+}
+
+const FullSizeImage = ({ imageSrc, altText }: { imageSrc: StaticImageData, altText: string }): JSX.Element => {
+  return (
+    <>
+      <Image
+        src={imageSrc}
+        style={{
+          width: "100vw",
+          height: "100vh",
+          minWidth: "100vw",
+          objectFit: "cover"
+        }}
+        alt={altText}
+      ></Image>
+    </>
+  )
 }
 
 export default Home;
